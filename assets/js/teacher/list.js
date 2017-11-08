8c6218e0bedadbabcf133dd2a14f7bb17a20d331
11Josey <2535051276@qq.com>
2017-11-08 19:35:43 +0800

    实现禁用和启用教师功能

--- assets/js/teacher/list.js
+++ assets/js/teacher/list.js
@@ -1,13 +1,13 @@
 /**教师列表项目
  * Created by Administrator on 2017/11/8.
  */
-define(['jquery','template','bootstrap'],function ($,template) {
+define(['jquery', 'template', 'bootstrap'], function ($, template) {
     $.ajax({
-        url:'/api/teacher',
-        type:'get',
-        success:function (data) {
+        url: '/api/teacher',
+        type: 'get',
+        success: function (data) {
             //console.log(data);
-            var tbodyHtml = template('teacherListTableTemplate',{list:data.result});
+            var tbodyHtml = template('teacherListTableTemplate', {list: data.result});
             $('#teacherListTable tbody').html(tbodyHtml);
 
         }
@@ -17,22 +17,22 @@ define(['jquery','template','bootstrap'],function ($,template) {
     // 但是如果使用事件委托,直接给table绑定点击事件,那么根据js的事件机制,只有点击的时候才会触发事件,那时候页面上已经存在点击按钮了
 
     //渲染查看讲师信息的模态框
-    $('#teacherListTable').on('click','.showInfo',function() {
-        var tc_id=$(this).parent().attr('data-tc-id');
+    $('#teacherListTable').on('click', '.showInfo', function () {
+        var tc_id = $(this).parent().attr('data-tc-id');
         $.ajax({
-        type:'get',
-        data:{tc_id:tc_id},
-        url:'/api/teacher/view',
-        success:function (info) {
-                if(info.code==200) {
+            type: 'get',
+            data: {tc_id: tc_id},
+            url: '/api/teacher/view',
+            success: function (info) {
+                if (info.code == 200) {
                     //定义过滤器, template.defaults.imports.funName=function(input) {return transform(input);}
                     //一定要先过滤,再调用template方法生成模板字符串,不然会报错,我的理解是替换规则都定义好了,最后才能根据规则生成模板字符串
                     //最后添加到页面中
                     //过滤器
-                    template.defaults.imports.space=function(data,arg1,arg2) {
-                        return data.replace(/\|/g,' ')+" "+arg1+arg2;//呵呵,我忘记了|是特殊字符,要转义
+                    template.defaults.imports.space = function (data, arg1, arg2) {
+                        return data.replace(/\|/g, ' ') ;//呵呵,我忘记了|是特殊字符,要转义
                     };
-                    var html=template('teacherListTableTpl2',info.result);
+                    var html = template('teacherListTableTpl2', info.result);
                     $('#teacherInfoTable tbody').html(html);
                     $('#teacherModal').modal('show');
                     //点击按钮弹出模态框,
@@ -42,5 +42,24 @@ define(['jquery','template','bootstrap'],function ($,template) {
         });
     });
 
+    //启用禁用按钮
+    $('#teacherListTable').on('click', '.status', function () {
+        var tc_id = $(this).parent('td').attr('data-tc-id');
+        var tc_status = $(this).attr('data-tc-status');
+        var that = this;
+        $.ajax({
+            url: '/api/teacher/handle',
+            type: 'post',
+            data: {
+                tc_id: tc_id,
+                tc_status: tc_status
+            },
+            success: function (info) {
+                console.log(info);
+                 $(that).attr('data-tc-status',info.result.tc_status).text(info.result.tc_status?'禁 用':'启 用');
 
+            }
+        });
+    });
 });
+
