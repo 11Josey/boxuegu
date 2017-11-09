d28a3a748c49fc821c480e511aba2e44b68277af
11Josey <2535051276@qq.com>
2017-11-09 17:05:55 +0800

    实现讲师编辑功能

--- assets/js/teacher/edit.js
+++ assets/js/teacher/edit.js
@@ -2,6 +2,36 @@
  * Created by Administrator on 2017/11/9.
  */
 //编辑讲师页面
-define(['jquery'],function($) {
+define(['jquery','template'],function($,template) {
+    var search = location.search;//?tc_id=4
+    var reg = /tc_id=(\d+)/ ;
+    var tc_id=reg.exec(search)[1];
+    $.ajax({
+        url:'/api/teacher/view',
+        type:'post',
+        data:{tc_id:tc_id},
+        success:function(info) {
+            //console.log(info);
+            if(info.code==200) {
+                var html=template('teacherEditTemplate',info.result);
+                $('#teacherEdit').html(html);
 
+            }
+        }
+    });
+    //
+    $('#teacherEdit').on('submit','#teacherEditForm',function(){
+        var formData = $('#teacherEditForm').serialize();
+       // console.log(formData);
+        $.ajax({
+            url:'/api/teacher/update',
+            type:'post',
+            data:formData,
+            success:function(info) {
+                //console.log(info);
+                location.pathname='/teacher/list';
+            }
+        });
+        return false;
+    });
 });
